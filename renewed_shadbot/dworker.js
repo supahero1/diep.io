831193e661a30434faad5609e5704cb1f23e70a0
shädam <47268949+supahero1@users.noreply.github.com>
2021-03-08 20:26:38 +0100

    Update dworker.js

--- renewed_shadbot/dworker.js
+++ renewed_shadbot/dworker.js
@@ -1,5 +1,3 @@
-const { parentPort } = require('worker_threads');
-
 function DecompressLZ4(packet) {
   const length = (packet[4] << 24) | (packet[3] << 16) | (packet[2] << 8) | packet[1];
   var at = 5;
@@ -51,7 +49,48 @@ function DecompressLZ4(packet) {
   return output;
 }
 
-parentPort.on('message', function(e) {
-  parentPort.postMessage([0, e[0]]);
-  parentPort.postMessage([1, e[0], DecompressLZ4(e[1])]);
+const Packet = function(packet) {
+  this.packet = packet;
+  this.at = 0;
+};
+Packet.prototype.readVU = function() {
+  var number = 0;
+  var count = 0;
+  do {
+    number |= (this.packet[this.at] & 0x7f) << (7 * count++);
+  } while((this.packet[this.at++] >> 7) == 1);
+  return number;
+};
+Packet.prototype.writeVU = function(num) {
+  let output = [];
+  while(num > 0x7f) {
+    output[output.length] = (num & 0x7f) | 0x80;
+    num >>>= 7;
+  }
+  output[output.length] = num;
+  return output;
+};
+
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8082 });
+
+let pending;
+
+server.on('connection', function(ws) {
+  if(pending != undefined) {
+    ws.send(pending);
+    pending = undefined;
+  }
+  ws.on('message', function(message) {
+    const data = new Packet(new Uint8Array(message));
+    const id = data.readVU();
+    const result = DecompressLZ4(data.packet.subarray(data.at));
+    if(ws.readyState == 1) {
+      ws.send(new Uint8Array([...data.writeVU(id), ...result]));
+    } else {
+      pending = new Uint8Array([...data.writeVU(id), ...result]);
+    }
+  });
+  ws.on('close', function() {});
+  ws.on('error', function() {});
 });
