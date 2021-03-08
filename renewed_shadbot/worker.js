446e837512f6a2a661bf4dffb4abc52ec94837fe
shädam <47268949+supahero1@users.noreply.github.com>
2021-03-08 20:25:46 +0100

    Update worker.js

--- renewed_shadbot/worker.js
+++ renewed_shadbot/worker.js
@@ -1,4 +1,3 @@
-const { parentPort } = require('worker_threads');
 (function() {
   "use strict";
   var root = "object" == typeof window ? window : {},
@@ -127,7 +126,51 @@ function checc(b, e) {
   return true;
 }
 const s = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-parentPort.on('message', function(e) {
-  parentPort.postMessage([0, e[0]]);
-  parentPort.postMessage([1, e[0], solve(e[1], e[2])]);
+
+const Packet = function(packet) {
+  this.packet = packet;
+  this.at = 0;
+};
+Packet.prototype.readVU = function() {
+  var number = 0;
+  var count = 0;
+  do {
+    number |= (this.packet[this.at] & 0x7f) << (7 * count++);
+  } while((this.packet[this.at++] >> 7) == 1);
+  return number;
+};
+Packet.prototype.writeVU = function(num) {
+  let output = [];
+  while(num > 0x7f) {
+    output[output.length] = (num & 0x7f) | 0x80;
+    num >>>= 7;
+  }
+  output[output.length] = num;
+  return output;
+};
+
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8081 });
+
+let pending;
+
+server.on('connection', function(ws) {
+  if(pending != undefined) {
+    ws.send(pending);
+    pending = undefined;
+  }
+  ws.on('message', function(message) {
+    const data = new Packet(new Uint8Array(message));
+    const id = data.readVU();
+    const difficulty = data.readVU();
+    const string = Array.from(data.packet.subarray(data.at)).map(r => String.fromCharCode(r)).join('');
+    const result = solve(string, difficulty);
+    if(ws.readyState == 1) {
+      ws.send(new Uint8Array([...data.writeVU(id), 10, ...result.split('').map(r => r.charCodeAt()), 0]));
+    } else {
+      pending = new Uint8Array([...data.writeVU(id), 10, ...result.split('').map(r => r.charCodeAt()), 0]);
+    }
+  });
+  ws.on('close', function() {});
+  ws.on('error', function() {});
 });
