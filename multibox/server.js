b63c431706a4cfb9a612d5cbf8df537c05768a25
shädam <47268949+supahero1@users.noreply.github.com>
2021-06-04 21:09:34 +0200

    Update server.js

--- multibox/server.js
+++ multibox/server.js
@@ -1,1 +1,15 @@
+"use strict";
 
+const WebSocket = require('ws');
+const Server = new WebSocket.Server({ port: 8090 });
+Server.on("connection", function(ws) {
+  ws.on("message", function(msg) {
+    Server.clients.forEach(function(client) {
+      if(client != ws && client.readyState == WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+  });
+  ws.on("error", function() {});
+  ws.on("close", function() {});
+});
